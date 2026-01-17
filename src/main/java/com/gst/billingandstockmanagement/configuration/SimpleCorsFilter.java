da8397f1f73f80ceea9fc3a022bb7258cf4ac098
tanaypatel1501 <tanaypatel1501@gmail.com>
2026-01-17 15:50:16 +0530

    Handled Cross-Origin Request Warning

--- src/main/java/com/gst/billingandstockmanagement/configuration/SimpleCorsFilter.java
+++ src/main/java/com/gst/billingandstockmanagement/configuration/SimpleCorsFilter.java
@@ -33,7 +33,7 @@ public class SimpleCorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", originHeader);
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
