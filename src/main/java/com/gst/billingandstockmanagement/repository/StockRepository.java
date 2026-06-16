021f4a3967aeaa441fba78df665bfc4a81c8a61c
tanaypatel1501 <tanaypatel1501@gmail.com>
2026-06-17 01:17:16 +0530

    stock service updated to handle mrp uniqueness

--- src/main/java/com/gst/billingandstockmanagement/repository/StockRepository.java
+++ src/main/java/com/gst/billingandstockmanagement/repository/StockRepository.java
@@ -6,17 +6,18 @@ import com.gst.billingandstockmanagement.entities.User;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface StockRepository extends JpaRepository<Stock, Long>, JpaSpecificationExecutor<Stock> {
 
-	Stock findByUserAndProduct(User user, Product product);
-	
 	List<Stock> findByUser(User user);
 
-	Stock findByUserAndProductAndBatchNoAndExpiryDate(User user, Product product, String batchNo, Date expiryDate);
+	Optional<Stock> findByUserAndProductAndBatchNoAndExpiryDateAndMrp(
+			User user, Product product, String batchNo, Date expiryDate, Double mrp
+	);
 
 	List<Stock> findByExpiryDateBetweenAndLastExpiryNotificationDateIsNull(Date start, Date end);
 
